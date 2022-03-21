573bbe05f9158c01348a99385f84470d11f56aa7
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-03-21 13:52:59 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContactCollection book = new ContactCollection();
+
+        for(ContactInfo a : book){
+            System.out.println(a);
+        }
+    }
+}
+
+class ContactInfo{
+    private String name;
+    private String email;
+
+    public ContactInfo(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactInfo{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
+class ContactCollection implements Iterable<ContactInfo>{
+    ArrayList<ContactInfo> myContactCollection = new ArrayList<>();
+
+    public ContactCollection() {
+        myContactCollection.add(new ContactInfo("Tom", "tom@gmail.com"));
+        myContactCollection.add(new ContactInfo("John", "john@gmail.com"));
+        myContactCollection.add(new ContactInfo("Sam", "sam@gmail.com"));
+    }
+
+    @Override
+    public Iterator iterator() {
+        return myContactCollection.iterator();
+    }
+
+    @Override
+    public String toString() {
+        return "ContactCollection{" +
+                "myContactCollection=" + myContactCollection +
+                '}';
+    }
+}
